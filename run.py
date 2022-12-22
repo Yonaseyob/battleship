252a26d9c6487746a85bc50eaac89e1b76d39a4a
Yonaseyob <yonaseyobberhane@gmail.com>
2022-12-22 09:47:58 +0000

    Add play game function

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import random
 
+ROWS = 5
+COLUMNS = 5
 
 def create_random_ship():
-    return random.randint(0, 5), random.randint(0, 5)
+    return random.randrange(ROWS), random.randrange(COLUMNS)
 
 
 def play_again():
-    try_again = input("Wanna play again? <Y>es or <N>o? >: ").lower()
+    try_again = input("Do you want to play again? <Y>es or <N>o? >: ").lower()
     if try_again == "y":
         play_game()
     else:
@@ -14,22 +16,13 @@ def play_again():
         return
 
 
-print("Welcome to the Battleship game!"
-      "\nYour main objective is to find and destroy all the hidden ships on map!\n")
+print("Welcome to the Battleship game!")
 
-print("""\nIntroductions:
-\nYou have 10 ammo and there are 3 hidden ships on map.
-In order to hit them, you have to enter specific numbers for that location. For example:
-For the first row and first column, you have to write 1 and 1.
-I wish you good fortune in wars to come!\n""")
+print("Lets play")
 
 
 def play_game():
-    game_board = [["O", "O", "O", "O", "O"],
-                  ["O", "O", "O", "O", "O"],
-                  ["O", "O", "O", "O", "O"],
-                  ["O", "O", "O", "O", "O"],
-                  ["O", "O", "O", "O", "O"]]
+    game_board = [["O"] * COLUMNS for _ in range(ROWS)]
 
     for i in game_board:
         print(*i)
@@ -38,42 +31,42 @@ def play_game():
     ship2 = create_random_ship()
     ship3 = create_random_ship()
     ships_left = 3
-    ammo = 10
+    gusse = 10
 
-    while ammo:
+    while gusse:
         try:
-            row = int(input("Enter a row number between 1-5 >: "))
-            column = int(input("Enter a column number between 1-5 >: "))
+            row = int(input("Enter a row"))
+            column = int(input("Enter a column"))
         except ValueError:
-            print("Only enter number!")
+            print("Only number between 1-5!")
             continue
 
         if row not in range(1,6) or column not in range(1, 6):
-            print("\nThe numbers must be between 1-5!")
+            print("The numbers must be between 1-5!")
             continue
 
-        row = row - 1 # Reducing number to desired index.
-        column = column - 1 # Reducing number to desired index.
+        row = row - 1 
+        column = column - 1 
 
         if game_board[row][column] == "-" or game_board[row][column] == "X":
-            print("\nYou have already shoot that place!\n")
+            print("You have already gussed")
             continue
         elif (row, column) == ship1 or (row, column) == ship2 or (row, column) == ship3:
-            print("\nBoom! You hit! A ship has exploded! You were granted a new ammo!\n")
+            print(" You got it that is a hit")
             game_board[row][column] = "X"
             ships_left -= 1
             if ships_left == 0:
-                print("My my, i didn't know you were a sharpshooter! Congratz, you won!")
+                print("Congradualations you win")
                 play_again()
         else:
-            print("\nYou missed!\n")
+            print("You missed!")
             game_board[row][column] = "-"
-            ammo -= 1
+            gusse -= 1
 
         for i in game_board:
             print(*i)
 
-        print(f"Ammo left: {ammo} | Ships left: {ships_left}")
+        print(f"Ammo left: {gusse} | Ships left: {ships_left}")
 
     play_again()
 
